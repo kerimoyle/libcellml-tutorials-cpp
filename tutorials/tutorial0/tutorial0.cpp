6c221e00ae42f5ebc5ea59685ffadce4ef9f235b
Keri Moyle <k.moyle@auckland.ac.nz>
2019-11-13 12:33:10 +0100

    Update tutorial0.cpp

--- tutorials/tutorial0/tutorial0.cpp
+++ tutorials/tutorial0/tutorial0.cpp
@@ -5,6 +5,8 @@
 
 int main()
 {
-  std::cout << "Hello libCellML user!" << std::endl;
-  std::cout << " The version number is "<<libcellml.version.versionHeading()<<std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << "    Welcome to libCellML!" << std::endl;
+    std::cout << "    This version number is " << libcellml::versionString() << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
 }
